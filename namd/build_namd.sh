3d7f83db5f292cc0c6ad1b248e7544c22fad171f
james-s-willis <james_willis@hotmail.co.uk>
2024-02-23 19:10:25 -0500

    Added bigger ICs for NAMD. Tiled STMV for 210 million atoms.

--- namd/build_namd.sh
+++ namd/build_namd.sh
@@ -15,7 +15,15 @@ tar xzf tcl8.6.13-linux-x86_64-threaded.tar.gz
 mv linux-x86_64/ fftw
 mv tcl8.6.13-linux-x86_64 tcl
 mv tcl8.6.13-linux-x86_64-threaded tcl-threaded
+
+# Compile one version with memopt
+./config Linux-x86_64-g++ --charm-arch mpi-linux-x86_64 --with-memopt
+cd Linux-x86_64-g++/
+make -j 6
+cd ..
+mv Linux-x86_64-g++ Linux-x86_64-g++-memopt
+
+# And another version without memopt
 ./config Linux-x86_64-g++ --charm-arch mpi-linux-x86_64
 cd Linux-x86_64-g++/
 make -j 6
-./namd3 
