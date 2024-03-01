9896896cb149e1424f9a3ea9d68ca62859a0bf6a
Erik Spence <ejspence@scinet.utoronto.ca>
2024-03-01 09:42:33 -0500

    Updated modules for the QE build instructions.

--- quantum-espresso/build_qe.sh
+++ quantum-espresso/build_qe.sh
@@ -3,10 +3,10 @@
 ## Instructions for building Quantum Espresso on Niagara.
 
 ## Load the required modules.
-module load NiaEnv/2022a
-module load gcc/11.3.0
-module load openmpi/4.1.4
-module load mkl
+module load NiaEnv/2019b
+module load gcc/8.3.0
+module load openmpi/4.1.4+ucx-1.11.2-mt
+module load mkl/2019u4
 
 ## Download the code.
 wget https://gitlab.com/QEF/q-e/-/archive/qe-7.3/q-e-qe-7.3.tar.gz
