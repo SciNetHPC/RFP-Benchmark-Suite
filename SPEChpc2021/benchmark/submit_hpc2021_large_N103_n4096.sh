ce6db6496210f3d1394df136a454125f5155eaa6
Bruno C. Mundim <bmundim@scinet.utoronto.ca>
2024-03-04 00:02:15 -0500

    Add gcc flags xml file. Required to have a valid run.

--- SPEChpc2021/benchmark/submit_hpc2021_large_N103_n4096.sh
+++ SPEChpc2021/benchmark/submit_hpc2021_large_N103_n4096.sh
@@ -13,5 +13,5 @@ cd $INSTALL_DIR
 source shrc
 cd $SPEC/config
 
-runhpc --config=gnu.cfg --ranks $SLURM_NTASKS --reportable --tune=base --pmodel MPI large
+runhpc --config=gnu.cfg --flagsurl=$SPEC/config/flags/gcc_flags.xml --ranks $SLURM_NTASKS --reportable --tune=base --pmodel MPI large
 
